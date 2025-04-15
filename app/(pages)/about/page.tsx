07673a8b025f6411802e2b233e859039c1fce393
Jakaria Chowdhury Tajwone <0562310005101031@neub.edu.bd>
2025-04-15 23:29:56 +0600

    Refactor About and Contact pages layout, enhance styling, and improve accessibility

--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -1,30 +1,37 @@
 import React from "react";
+import { MdInfoOutline } from "react-icons/md"; // Icon import
 
 export default function About() {
   return (
-    <div>
-      <h1 className="mt-7 text-center text-3xl font-extrabold">About Us</h1>
-      <div className="mx-auto mt-10 max-w-3xl px-4">
-        <p className="mt-7 text-lg font-normal">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod nostrum
-          non delectus, ad quasi exercitationem assumenda nam excepturi alias
-          molestiae fugit sunt illo ullam odit quisquam blanditiis temporibus,
-          laudantium tempora minus, tenetur mollitia libero ea. Ipsam,
-          consequatur. Voluptatibus, at tempore distinctio amet consequuntur
-          repudiandae natus id sunt quas, unde magni? Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Cum, repellendus? Quasi exercitationem,
-          saepe reiciendis placeat assumenda et ullam voluptas aperiam. Quo
-          aperiam eum facilis consequuntur laboriosam assumenda distinctio
-          perferendis nostrum! Mollitia at tempora, perferendis fugit corporis
-          tempore repellat sapiente eligendi. Sunt corrupti dolor magnam
-          inventore, deleniti porro distinctio temporibus eaque? Id placeat sint
-          aspernatur aut, recusandae excepturi! Provident, facere quam. Voluptas
-          error dolor quis corporis quae consequatur neque minus ipsum. Dolorem
-          mollitia recusandae cumque unde explicabo. Distinctio repellat non
-          deleniti! Saepe voluptatum recusandae dolores iure nostrum aliquid,
-          amet esse expedita. Velit eligendi illum pariatur minus aliquam
-          deleniti perspiciatis excepturi nulla?
-        </p>
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="w-full max-w-4xl rounded-lg border-2 border-white p-8">
+        <div className="mb-6 flex items-center justify-center gap-2">
+          <MdInfoOutline size={30} color="#92e3a9" />
+          <h1 className="text-3xl font-extrabold">About Us</h1>
+        </div>
+        <div className="mx-auto max-w-3xl px-4">
+          <p className="text-justify text-lg font-normal">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+            nostrum non delectus, ad quasi exercitationem assumenda nam
+            excepturi alias molestiae fugit sunt illo ullam odit quisquam
+            blanditiis temporibus, laudantium tempora minus, tenetur mollitia
+            libero ea. Ipsam, consequatur. Voluptatibus, at tempore distinctio
+            amet consequuntur repudiandae natus id sunt quas, unde magni? Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Cum, repellendus?
+            Quasi exercitationem, saepe reiciendis placeat assumenda et ullam
+            voluptas aperiam. Quo aperiam eum facilis consequuntur laboriosam
+            assumenda distinctio perferendis nostrum! Mollitia at tempora,
+            perferendis fugit corporis tempore repellat sapiente eligendi. Sunt
+            corrupti dolor magnam inventore, deleniti porro distinctio
+            temporibus eaque? Id placeat sint aspernatur aut, recusandae
+            excepturi! Provident, facere quam. Voluptas error dolor quis
+            corporis quae consequatur neque minus ipsum. Dolorem mollitia
+            recusandae cumque unde explicabo. Distinctio repellat non deleniti!
+            Saepe voluptatum recusandae dolores iure nostrum aliquid, amet esse
+            expedita. Velit eligendi illum pariatur minus aliquam deleniti
+            perspiciatis excepturi nulla?
+          </p>
+        </div>
       </div>
     </div>
   );
--- app/(pages)/contact/page.tsx
+++ app/(pages)/contact/page.tsx
@@ -1,59 +1,74 @@
 "use client";
 
 import { Button, Label, Textarea, TextInput } from "flowbite-react";
+import { MdContactMail } from "react-icons/md"; // Icon import
 
 export default function Component() {
   return (
-    <>
-      <form action="#">
-        <div className="mt-3 mb-6">
-          <Label htmlFor="email" className="mb-2 block">
-            Your email
-          </Label>
-          <TextInput
-            id="email"
-            name="email"
-            placeholder="name@company.com"
-            type="email"
-          />
+    <div className="flex h-screen items-center justify-center">
+      <div className="flex w-full max-w-4xl flex-col items-center rounded-lg border-2 border-white p-8">
+        <div className="mb-6 flex items-center gap-2">
+          <MdContactMail size={30} color="#92e3a9" />
+          <h1 className="text-3xl font-extrabold">Contact</h1>
         </div>
-        <div className="mb-6">
-          <Label htmlFor="subject" className="mb-2 block">
-            Subject
-          </Label>
-          <TextInput
-            id="subject"
-            name="subject"
-            placeholder="Let us know how we can help you"
-          />
-        </div>
-        <div className="mb-6">
-          <Label htmlFor="message" className="mb-2 block">
-            Your message
-          </Label>
-          <Textarea
-            id="message"
-            name="message"
-            placeholder="Your message..."
-            rows={4}
-          />
-        </div>
-        <div className="mb-6">
-          <Button type="submit" className="w-full">
-            Send message
-          </Button>
-        </div>
-        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-          <a href="mailto:info@company.com" className="hover:underline">
-            info@company.com
-          </a>
-        </p>
-        <p className="text-sm text-gray-500 dark:text-gray-400">
-          <a href="tel:2124567890" className="hover:underline">
-            212-456-7890
-          </a>
-        </p>
-      </form>
-    </>
+        <form action="#" className="w-full max-w-3xl">
+          <div className="mb-6">
+            <Label htmlFor="email" className="mb-2 block">
+              Your email
+            </Label>
+            <TextInput
+              id="email"
+              name="email"
+              placeholder="name@company.com"
+              type="email"
+            />
+          </div>
+          <div className="mb-6">
+            <Label htmlFor="subject" className="mb-2 block">
+              Subject
+            </Label>
+            <TextInput
+              id="subject"
+              name="subject"
+              placeholder="Let us know how we can help you"
+            />
+          </div>
+          <div className="mb-6">
+            <Label htmlFor="message" className="mb-2 block">
+              Your message
+            </Label>
+            <Textarea
+              id="message"
+              name="message"
+              placeholder="Your message..."
+              rows={4}
+            />
+          </div>
+          <div className="mb-6">
+            <Button
+              style={{
+                backgroundColor: "#92e3a9",
+                color: "#000000",
+                marginTop: "20px",
+              }}
+              type="submit"
+              className="w-full"
+            >
+              Send message
+            </Button>
+          </div>
+          <p className="mb-2 text-center text-sm text-gray-500 dark:text-gray-400">
+            <a href="mailto:info@company.com" className="hover:underline">
+              North East University Bangladesh
+            </a>
+          </p>
+          <p className="text-center text-sm text-gray-500 dark:text-gray-400">
+            <a href="tel:2124567890" className="hover:underline">
+              212-456-7890
+            </a>
+          </p>
+        </form>
+      </div>
+    </div>
   );
 }
