caf4c6f28a1f9963d847fd8b7b683944c5e8f82f
Jakaria Chowdhury Tajwone <0562310005101031@neub.edu.bd>
2025-05-20 16:35:42 +0600

    Setup db

--- app/api/departments/route.ts
+++ app/api/departments/route.ts
@@ -1,7 +1,6 @@
 import db from "@/app/lib/db";
-import { NextRequest } from "next/server";
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   const [results] = await db.query("SELECT * FROM department");
   return new Response(JSON.stringify({ departments: results }), {
     status: 200,
